46a47e86b5f8635ab857578b9b0d8b4c6906ee32
AMOGUSUSUS <92963088+AMOGUSUSUS@users.noreply.github.com>
2026-07-24 21:37:27 -0400

    fix null demon level

--- app/api/submissions/route.ts
+++ app/api/submissions/route.ts
@@ -7,23 +7,27 @@ export async function POST(req: NextRequest) {
   try {
     const { playerName, demonName, videoUrl } = await req.json();
 
-    if (!playerName || !demonName || !videoUrl) {
+    // Check only for player name and level name (video is optional)
+    if (!playerName || !demonName) {
       return NextResponse.json(
-        { error: "Missing required fields." },
+        { error: "Player name and demon name are required." },
         { status: 400 }
       );
     }
 
+    // Insert into both level_name and demon_name to ensure Neon catches it regardless of column naming
     await sql`
       INSERT INTO submissions (
         player_name,
+        level_name,
         demon_name,
         video_url
       )
       VALUES (
         ${playerName},
         ${demonName},
-        ${videoUrl}
+        ${demonName},
+        ${videoUrl || null}
       )
     `;
 
@@ -32,7 +36,7 @@ export async function POST(req: NextRequest) {
       { status: 201 }
     );
   } catch (error) {
-    console.error(error);
+    console.error("Submission Error:", error);
 
     return NextResponse.json(
       { error: "Internal Server Error" },
